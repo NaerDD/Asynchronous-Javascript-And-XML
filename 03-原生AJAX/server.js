324c5e39b74090620f6f4687a741c286a13b4be6
NaerDD <806915230@qq.com>
2022-12-26 20:37:32 +0800

    commit 3

--- 03-原生AJAX/server.js
+++ 03-原生AJAX/server.js
@@ -7,15 +7,18 @@ const app = express();
 //3、创建路由规则
 //request是对请求报文的封装
 //response 是对响应报文的封装
-app.get('/',(request,response)=>{
+app.get('/server',(request,response)=>{
  
     // //设置响应头  设置允许跨域
-    // response.setHeader('Access-Control-Allow-Origin','*')
-    // //设置响应体
-    // response.send('hello ajax get')
-    response.send('HELLO EXPRESS ')
-    
+    response.setHeader('Access-Control-Allow-Origin','*')
+    // 设置响应体
+    response.send('HELLO AJAX')
 })
+//4、监听端口启动服务
+app.listen(8000,()=>{
+    console.log('服务已经启动，8000端口监听中。。。');
+})
+
 // app.all('/server',(request,response)=>{
  
 //     //设置响应头  设置允许跨域
@@ -26,7 +29,6 @@ app.get('/',(request,response)=>{
     
 // })
  
-
 
 // app.all('/jquery-jsonp-server',(request,response)=>{
  
@@ -40,7 +42,3 @@ app.get('/',(request,response)=>{
 
 //     response.end(`${cb}(${str})`);
 // });
-//4、监听端口启动服务
-app.listen(8000,()=>{
-    console.log('服务已经启动，8000端口监听中。。。');
-})
